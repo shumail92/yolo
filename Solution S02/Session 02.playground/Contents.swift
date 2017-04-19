0590ef5603ba0d52c95d7ed0a37648aa39afadc3
Shumail Mohyuddin <shumail.mohyuddin@tum.de>
2017-04-19 16:12:08 +0200

    Fix: changed variable to constant.

--- Solution S02/Session 02.playground/Contents.swift
+++ Solution S02/Session 02.playground/Contents.swift
@@ -1,7 +1,7 @@
 //: Playground - noun: a place where people can play
 import UIKit
 
-var myName: String = "Shumail Mohyuddin"
+let myName: String = "Shumail Mohyuddin"
 
 // Parameters should have proper names, x1 is maybe not that good of a choice :)
 func greet(recipient: String) {
